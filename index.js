26bab00ccab8f24d3f21ce60f59e5e000a78716c
Cooper <55swimmer55@gmail.com>
2023-08-23 09:17:24 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    const winningObject = array.find(obj => obj.result === "W");
+    if (winningObject) {
+      return winningObject.year;
+    } else {
+      return undefined;
+    }
+  }
